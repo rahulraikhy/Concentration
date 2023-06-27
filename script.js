31bd6e2608584e8e010cd654cf8f8b1e21234f9d
rahulraikhy <rlraikhy@gmail.com>
2023-06-28 00:05:31 +0100

    all images added, major code changes

--- script.js
+++ script.js
@@ -1,48 +1,118 @@
 /*----- constants -----*/
-
+const moves = document.getElementById("moves-count");
+const timeValue = document.getElementById("time");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const gameContainer = document.querySelector("game-container");
+const result = document.getElementById("result");
+const controls = document.querySelector("controls-container");
 
 /*----- state variables -----*/
-let board;
-let winner; //winner or loser when passed a certain number of turns (60?)
+let cards;
+let interval;
+let firstCard = false;
+let secondCard = false;
 
 /*----- cached elements  -----*/
-
-
-/*----- event listeners -----*/
+const items = [
+    {name: "a", Image: "letter-a.png"},
+    {name: "b", Image: "letter-b.png"},
+    {name: "c", Image: "letter-c.png"},
+    {name: "d", Image: "letter-d.png"},
+    {name: "e", Image: "letter-e.png"},
+    {name: "f", Image: "letter-f.png"},
+    {name: "g", Image: "letter-g.png"},
+    {name: "h", Image: "letter-h.png"},
+    {name: "i", Image: "letter-i.png"},
+    {name: "j", Image: "letter-j.png"},
+    {name: "k", Image: "letter-k.png"},
+    {name: "l", Image: "letter-l.png"},
+];
 
 
 /*----- functions -----*/
-init();
+let seconds = 0, minutes = 0;
 
-function init() {
-	board = [
-		[0, 0, 0, 0], //col 0
-		[0, 0, 0, 0], //col 1
-		[0, 0, 0, 0], //col 2
-		[0, 0, 0, 0], //col 3
-		[0, 0, 0, 0], //col 4
-		[0, 0, 0, 0], //col 5
-	];
-	winner = null;
-	render();
-}
+let movesCount = 0, winCount = 0;
 
-function render() {
-	renderBoard();
-	renderMessage();
-	renderControls();
-}
+const timeGenerator = () => {
+    seconds =+ 1;
 
-function renderBoard() {
-  board.forEach(function(colArr, colIdx) {
-    console.log(colIdx, colArr);
-  });
-}
+    if (seconds >= 60) {
+        minutes += 1;
+        seconds = 0;
+    }
 
-function renderMessage() {
+    let secondsValue = seconds < 10 ? `0{seconds}` : seconds;
+    let minutesValue = minutes < 10 ? `0{minutes}` : minutes;
+    timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+};
 
-}
+const movesCounter = () => {
+    movesCount += 1;
+    moves.innerHTML = `<span>Moves:</span>${movesCount}`
+};
 
-function renderControls() {
-	
-}
+const generateRandom = (size = 4) => {
+    let tempArray = [...items];
+    let cardValues = [];
+    size = (size * size) / 2;
+
+    for(let i = 0; i < size; i++) {
+        const randomIndex = Math.floor(Math.random() * tempArray.length);
+        cardValues.push(tempArray[randomIndex]);
+        tempArray.splice(randomIndex, 1);
+    }
+    return cardValues;   
+};
+
+const matrixGenerator = (cardValues, size = 4) => {
+    gameContainer.innerHTML = " ";
+    cardValues = [...cardValues, ...cardValues];
+    cardValues.sort(() => Math.random() - 0.5);
+    for (let i = 0; i < size * size; i++) {
+        gameContainer.innerHTML += `
+        <div class="card-container" data-card-value="${cardValues[i].name}">
+            <div class="card-before">?</div>
+            <div class="card-after">
+            <img src="${cardValues[i].image}"class="image"/></div>
+        </div>
+        `;
+    }
+    gameContainer.style.gridTemplateColumns = `repeat(${size}, auto)`;
+
+    cards = document.querySelectorAll(".card-container");
+    cards.forEach((card) => { 
+        card.addEventListener("click", () => {
+            if(!card.classList.contains("matched")) {
+                card.classList.add("flipped");
+                if(!firstCard) {
+                    firstCard = card;
+                    firstCardValue = card.getAttribute("data-card-value");
+                }
+            }
+            else{
+                movesCounter();
+                secondCard = card;
+                let secondCardValue = card.getAttribute("data-card-value");
+                if(firstCard === secondCardValue) {
+                    firstCard.classList.add("matched");
+                    secondCard.classList.add("matched");
+                    first
+                } else {
+
+                }
+            }
+        })
+    })
+};
+
+const initializer = () => {
+    result.innerText = " ";
+    winCount = 0;
+    let cardValues = generateRandom();
+    console.log(cardValues);
+    matrixGenerator(cardValues);
+};
+
+initializer(); 
